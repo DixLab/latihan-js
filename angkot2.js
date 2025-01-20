117a5ea8b0547878a87153ea60d1f0c7e00fa3cb
D I X I E <dwinandika41@gmail.com>
2025-01-20 16:53:10 +0800

    Menambah file angkot2.js

---
+++ angkot2.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 8;
+var noAngkot = 1;
+
+while(noAngkot <= angkotBeroperasi) {
+    console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+    noAngkot++;
+}
+
+for(angkotBeroperasi; angkotBeroperasi < jmlAngkot; angkotBeroperasi++){
+    console.log(`Angkot No. ${angkotBeroperasi+1} sedang tidak beroperasi.`);
+}
